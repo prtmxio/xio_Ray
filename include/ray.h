aa63fe9492795baff87eba83f185c9a64f202867
Pritam <dhimanpritam1579@gmail.com>
2025-09-10 01:04:20 +0530

    Feat: added ray.h

---
+++ include/ray.h
@@ -0,0 +1,21 @@
+#ifndef RAY_H
+#define RAY_H
+#include "vec3.h"
+
+class ray {
+  private:
+    point orig;
+    vec3 dir;
+
+  public:
+    ray() {}
+    ray(const point &origin, const vec3 &direction)
+        : orig(origin), dir(direction) {}
+
+    point origin() const { return orig; }
+    vec3 direction() const { return dir; }
+
+    point at(double t) const { return orig + t * dir; }
+};
+
+#endif
